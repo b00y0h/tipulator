db31b222d893a9988d4313a101c1c67f305df9c4
Bobby Smith <bob.smith@capitalone.com>
2016-01-25 18:44:48 -0500

    init commit

--- tipulator/ViewController.swift
+++ tipulator/ViewController.swift
@@ -10,16 +10,68 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tipLabel: UILabel!
+    
+    @IBOutlet weak var billField: UITextField!
+    
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    @IBOutlet weak var tipStepperLabel: UILabel!
+    
+    @IBOutlet weak var stepper: UIStepper!
+    
+    
+    @IBAction func stepperValueChanged(sender: AnyObject) {
+        tipStepperLabel.text = Int(stepper.value).description
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // initialize values
+        tipLabel.text = "$0.00"
+        totalLabel.text = "$0.00"
+        tipStepperLabel.text = Int(stepper.value).description
+        
     }
+    
+    @IBAction func changeAmount(sender: AnyObject) {
+        
+        // grab value from stepper and turn in to a decimal/percent
+        let tipPercentage = Double(stepper.value) / 100
+        var tip:Double = 0
+        var total:Double = 0
+        
+        // grab bill amount
+        let billAmount = Double(billField.text!)
+
+        // don't crash on nil
+        if (billAmount != nil) {
+            // calculate tip
+            tip = billAmount! * tipPercentage
+            // calculate total bill
+            total = billAmount! + tip
+        }
+        
+        // write to screen
+        tipLabel.text = "\(tip.asLocaleCurrency)"
+        totalLabel.text = "\(total.asLocaleCurrency)"
+        }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
 }
 
+extension Double {
+    // set double to local currency string
+    var asLocaleCurrency:String {
+        let formatter = NSNumberFormatter()
+        formatter.numberStyle = .CurrencyStyle
+        formatter.locale = NSLocale.currentLocale()
+        return formatter.stringFromNumber(self)!
+    }
+
+}
